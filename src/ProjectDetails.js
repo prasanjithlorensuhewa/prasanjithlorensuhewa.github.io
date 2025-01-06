1d923222bbc8417a12864e9ab784c74a74c9cf12
Prasanjith76 <prasanjith123.lorensuhewa@gmail.com>
2025-01-06 15:18:48 +0530

    updated ProjectDetails

--- src/ProjectDetails.js
+++ src/ProjectDetails.js
@@ -1,5 +1,23 @@
 export const projectDetails = [
     {
+        title: "Safe RL algorithm for T1D Patients (FYP)",
+        image: `${process.env.PUBLIC_URL}/images/project/VendorEcommerce.png`,
+        description: `Enhances safety in insulin treatment for Type 1 diabetes patients by
+ integrating safety based reinforcement learning algorithm meeting
+ clinical safety standards.`,
+        techstack: "Python, PyTorch, Reinforcement Learning",
+        githubLink: "https://github.com/RL4HCPO/RL4T1D",
+    },
+    {
+        title: "Room Reservation Management System",
+        image: `${process.env.PUBLIC_URL}/images/project/VendorEcommerce.png`,
+        description: ` Comprehensive hotel management platform with secure
+ authentication, seamless room management, and reservation handling
+ for admins and customers`,
+        techstack: "Angular, Spring Boot, MySQL, Ng Zorro",
+        githubLink: "https://github.com/Room-Reservation-Management-System",
+    },
+    {
         title: "BookWizApp - Automated Book summary and Q/A generating system",
         image: `${process.env.PUBLIC_URL}/images/project/BookWizApp.jpg`,
         description: `Our integrated system begins by allowing users to select a book from our extensive catalog, sourced directly from the Gutenberg site. Once a book is chosen, our chatbot, utilizing Bart and Pegasus models, generates a comprehensive summary of the selected book. But the interaction doesn't end there; our Q&A chatbot, powered by the advanced Llama 13-b model, stands ready to answer any questions users may have about the book's content, enriching the reading experience with insightful and personalized insights.`,
@@ -19,6 +37,7 @@ export const projectDetails = [
         techstack: "PHP, HTML, Bootstrap, JS, Ajax",
         githubLink: "https://github.com/prasanjithlorensuhewa/Ecommerce_DBMS",
     },
+  
     {
         title: "Food Ordering Web App (Ongoing)",
         image: `${process.env.PUBLIC_URL}/images/project/BookWizApp.jpg`,
