7bfd0f49c2141477f6761c322a9f09fd6c80882b
Prasanjith76 <prasanjith123.lorensuhewa@gmail.com>
2025-01-16 23:17:34 +0530

    updated cv

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -60,7 +60,7 @@ const Home = () => {
     <div className='main-btn'>
       <Link to={'/contact'} className='btn'>Hire Me</Link>
       <a 
-              href='https://drive.google.com/file/d/1uzLnROq5IcqN97RABCFg53-Z64DUb8mi/view?usp=sharing' 
+              href='https://drive.google.com/file/d/1KQ6IM9COzygPskbyeUCIV9HZLoAyxezF/view?usp=sharing' 
               className='btn btn2'
               download
               target='_blank'
